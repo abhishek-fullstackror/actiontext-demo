6fc04ea800c7a39df6f59557c563968e765746d8
Abhishek <abhishekgangdhari@gmail.com>
2024-10-06 18:08:48 +0530

    MOVE Existing Body Content

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+100.times do
+    User.create(name: Faker::Name.name, body: Faker::Quote.matz)
+end
